6dae69aa1ab2e2740bbb2a589e4f2aa4999bbd47
Vickixiaodong <xiexiaodong917@163.com>
2017-10-30 20:45:17 +0800

    add 14 of moretypes

---
+++ moretypes/14/range-continued.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	pow := make([]int, 10)
+
+	for i := range pow {
+		pow[i] = 1 << uint(i)
+	}
+
+	for _, value := range pow {
+		fmt.Printf("%d\n", value)
+	}
+}
+
+/*
+1
+2
+4
+8
+16
+32
+64
+128
+256
+512
+*/
