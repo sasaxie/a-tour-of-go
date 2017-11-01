87c9505b14a39307ce35c49c16dc527172b3ceb9
Vickixiaodong <xiexiaodong917@163.com>
2017-11-01 20:36:44 +0800

    add 8 of methods

---
+++ methods/8/errors.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type MyError struct {
+	When time.Time
+	What string
+}
+
+func (e *MyError) Error() string {
+	return fmt.Sprintf("at %v, %s", e.When, e.What)
+}
+
+func run() error {
+	return &MyError{
+		time.Now(),
+		"it didn't work",
+	}
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Println(err)
+	}
+}
+
+/*
+at 2017-11-01 20:36:14.521733264 +0800 CST m=+0.000235335, it didn't work
+*/
