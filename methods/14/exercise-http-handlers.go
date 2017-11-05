0a8f570aa761d460d2452f054d943d2d223db1ff
Vickixiaodong <xiexiaodong917@163.com>
2017-11-05 23:49:37 +0800

    add 14 of methods

---
+++ methods/14/exercise-http-handlers.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type String string
+
+func (s String) ServeHTTP(
+	w http.ResponseWriter,
+	r *http.Request) {
+	fmt.Fprintf(w, "%s", s)
+}
+
+type Struct struct {
+	Greeting string
+	Punct    string
+	Who      string
+}
+
+func (s Struct) ServeHTTP(
+	w http.ResponseWriter,
+	r *http.Request) {
+	fmt.Fprintf(w, "%s%s%s", s.Greeting, s.Punct, s.Who)
+}
+
+func main() {
+	// your http.Handle calls here
+	http.Handle("/string", String("I'm a frayed knot."))
+	http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
+
+	log.Fatal(http.ListenAndServe("localhost:4000", nil))
+}
